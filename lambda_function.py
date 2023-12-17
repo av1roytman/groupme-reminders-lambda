6ad8ea48b581e6560ecb134f2619832c06c44cbf
av1roytman <aroytman@purdue.edu>
2023-12-16 22:39:29 -0600

    should work

--- lambda_function.py
+++ lambda_function.py
@@ -5,23 +5,16 @@ def lambda_handler(event, context):
     # Your GroupMe bot ID
     bot_id = "b509b2b87ca46be9b0cddaaec6"
 
-    # Parse the incoming event
-    print("Received event: " + json.dumps(event))
-    message_data = json.loads(event['body'])
-    print(message_data)
-    print(message_data.get('sender_type'))
-    print(message_data.get('sender_id'))
-
     # Check if the sender is not the bot itself
     # Assuming the message data includes sender_type or sender_id
-    if message_data.get('sender_type') != 'bot' and message_data.get('sender_id') != bot_id:
+    if event['sender_id'] != bot_id:
         # GroupMe API endpoint for posting messages
         groupme_post_message_url = "https://api.groupme.com/v3/bots/post"
 
         # Prepare the response message
         response_message = {
             "bot_id": bot_id,
-            "text": "Hello"
+            "text": "Hello Cracka"
         }
 
         # Send the message
