ae553cc7b07742ed137851855ae872c2d1af8301
av1roytman <aroytman@purdue.edu>
2023-12-28 20:00:49 -0600

    made some changes to the inputs to api

--- lambda_function.py
+++ lambda_function.py
@@ -53,6 +53,14 @@ def generate_response_and_send_message(style_message, input_message):
         traceback.print_exc()
         message = "Sorry, I couldn't process that."
 
+def send_message(message):
+    response_message = {
+        "bot_id": bot_id,
+        "text": message
+    }
+
+    # Send the message
+    requests.post(groupme_post_message_url, json=response_message)
 
 def lambda_handler(event, context):
     # Your GroupMe bot ID
@@ -61,18 +69,21 @@ def lambda_handler(event, context):
 
     # Check if message contains "Luke Butt" using regex
     if event['sender_id'] != bot_id and event['sender_type'] != "bot":
-        random_numer = random.randint(0, 10)
+        random_numer = random.randint(0, 50)
 
         if event['sender_id'] == omar_user_id:
-            generate_response_and_send_message("You are a creative, imaginative, and rude old man named Uncle Sherwin.", \
-                                               event['name'] + " is a fictional misbehaving child. Say what you would say to him to discipline him.")
+            generate_response_and_send_message("Respond to the user's input with humor. Use light-hearted and witty humor, appropriate for a general audience. " + \
+                                               "Incorporate playful language, puns, and clever wordplay where possible. Aim to entertain and amuse without being " + \
+                                               "offensive. If the user's input is about a specific topic, make humorous connections or jokes related to that topic. " + \
+                                               "Avoid sarcasm or irony that could be misunderstood. Keep the tone friendly and engaging.", \
+                                               event['name'] + "said: " + event['text'])
 
         elif re.search(r"luke butt", event['text'].lower()):
-            generate_response_and_send_message("You are a creative and imaginative comedian that is slightly evil.", event['text'])
+            generate_response_and_send_message("You are helpful assistant with a sense of humor", event['text'])
 
         # Check if the sender is not the bot itself
         # Assuming the message data includes sender_type or sender_id
-        elif random_numer > 9:
+        elif random_numer > 48:
             # GroupMe API endpoint for posting messages
 
             # Send the message
