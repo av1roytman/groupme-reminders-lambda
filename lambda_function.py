d65f3a129eaad71d49a1b4b4efe310c70b190d74
av1roytman <aroytman@purdue.edu>
2023-12-31 19:23:57 -0600

    printing for test

--- lambda_function.py
+++ lambda_function.py
@@ -4,8 +4,10 @@ import random
 import traceback
 import os
 import re
+import boto3
 
 bot_id = os.environ['BOT_ID']
+table_name = os.environ['TABLE_NAME']
 groupme_post_message_url = "https://api.groupme.com/v3/bots/post"
 
 def send_message(message):
@@ -18,6 +20,16 @@ def send_message(message):
     requests.post(groupme_post_message_url, json=response_message)
 
 def lambda_handler(event, context):
+    # Get all reminders from dynamoDB table
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(table_name)
+    response = table.scan()
+    reminders = response['Items']
+
+    # Print all reminders
+    for reminder in reminders:
+        print(reminder)
+    
 
     return {
         'statusCode': 200,
